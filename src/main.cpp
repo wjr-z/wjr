b73cee9f36fba9e6fbde17197a0b515f9918812c
wjrwjr <1966336874@qq.com>
2022-04-05 13:57:13 +0800

    remove huffman and deflate

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 ﻿#include <functional>
 #include "../include/action.h"
-#include "../include/deflate.h"
 #include "../include/exist_ptr.h"
 #include "../include/graphic.h"
 #include "../include/json.h"
