d456b5d8f6bf06588b7e7fe333beec91d87df9e2
wjr <1966336874@qq.com>
2023-12-14 16:14:30 +0800

    save

--- main.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include <wjr/preprocessor.hpp>
-int main() {
-#define WJR_PP_FUNC_INLINE WJR_PP_TABLE_DEF()
-    std::cout << WJR_PP_STR((WJR_PP_IS_TABLE(WJR_PP_FUNC_, INLINE))) << '\n';
-    return 0;
-}
