aa7f8485b1fce2d8dded0c92493aee2a761918cb
wjr <1966336874@qq.com>
2024-02-24 13:00:07 +0800

    optimize pipeline

--- include/wjr/x86/mul.hpp
+++ include/wjr/x86/mul.hpp
@@ -254,25 +254,25 @@ inline uint64_t asm_addmul_1(uint64_t *dst, const uint64_t *src, size_t n, uint6
 
         ".Ll2%=:\n\t"
         "mulx{q (%[src]), %[r10], %[r11]| %[r11], %[r10], [%[src]]}\n\t"
-        "lea{q -48(%[src]), %[src]| %[src], [%[src] - 48]}\n\t"
+        "lea{q 16(%[src]), %[src]| %[src], [%[src] + 16]}\n\t"
         "lea{q -48(%[dst]), %[dst]| %[dst], [%[dst] - 48]}\n\t"
         "jmp .Lb2%=\n\t"
 
         ".Ll3%=:\n\t"
         "mulx{q (%[src]), %[r8], %[r9]| %[r9], %[r8], [%[src]]}\n\t"
-        "lea{q -40(%[src]), %[src]| %[src], [%[src] - 40]}\n\t"
+        "lea{q 24(%[src]), %[src]| %[src], [%[src] + 24]}\n\t"
         "lea{q -40(%[dst]), %[dst]| %[dst], [%[dst] - 40]}\n\t"
         "jmp .Lb3%=\n\t"
 
         ".Ll4%=:\n\t"
         "mulx{q (%[src]), %[r10], %[r11]| %[r11], %[r10], [%[src]]}\n\t"
-        "lea{q -32(%[src]), %[src]| %[src], [%[src] - 32]}\n\t"
+        "lea{q 32(%[src]), %[src]| %[src], [%[src] + 32]}\n\t"
         "lea{q -32(%[dst]), %[dst]| %[dst], [%[dst] - 32]}\n\t"
         "jmp .Lb4%=\n\t"
 
         ".Ll5%=:\n\t"
         "mulx{q (%[src]), %[r8], %[r9]| %[r9], %[r8], [%[src]]}\n\t"
-        "lea{q -24(%[src]), %[src]| %[src], [%[src] - 24]}\n\t"
+        "lea{q 40(%[src]), %[src]| %[src], [%[src] + 40]}\n\t"
         "lea{q -24(%[dst]), %[dst]| %[dst], [%[dst] - 24]}\n\t"
         "jmp .Lb5%=\n\t"
 
@@ -311,11 +311,10 @@ inline uint64_t asm_addmul_1(uint64_t *dst, const uint64_t *src, size_t n, uint6
 
         ".Lb0%=:\n\t"
         "mulx{q 8(%[src]), %[r8], %[r9]| %[r9], %[r8], [%[src] + 8]}\n\t"
-        "adcx{q (%[dst]), %[r10]| %[r10], [%[dst]]}\n\t"
-        "adox{q %[r11], %[r8]| %[r8], %[r11]}\n\t"
-        "mov{q %[r10], (%[dst])| [%[dst]], %[r10]}\n\t"
-
         "lea{q -1(%[cx]), %[cx]| %[cx], [%[cx] - 1]}\n\t"
+        "adox{q %[r11], %[r8]| %[r8], %[r11]}\n\t"
+        "adcx{q (%[dst]), %[r10]| %[r10], [%[dst]]}\n\t"
+        "mov{q %[r10], (%[dst])| [%[dst]], %[r10]}\n\t"
 
         ".Lb7%=:\n\t"
         "mulx{q 16(%[src]), %[r10], %[r11]| %[r11], %[r10], [%[src] + 16]}\n\t"
@@ -325,35 +324,35 @@ inline uint64_t asm_addmul_1(uint64_t *dst, const uint64_t *src, size_t n, uint6
 
         ".Lb6%=:\n\t"
         "mulx{q 24(%[src]), %[r8], %[r9]| %[r9], %[r8], [%[src] + 24]}\n\t"
-        "adcx{q 16(%[dst]), %[r10]| %[r10], [%[dst] + 16]}\n\t"
+        "lea{q 64(%[src]), %[src]| %[src], [%[src] + 64]}\n\t"
         "adox{q %[r11], %[r8]| %[r8], %[r11]}\n\t"
+        "adcx{q 16(%[dst]), %[r10]| %[r10], [%[dst] + 16]}\n\t"
         "mov{q %[r10], 16(%[dst])| [%[dst] + 16], %[r10]}\n\t"
 
         ".Lb5%=:\n\t"
-        "mulx{q 32(%[src]), %[r10], %[r11]| %[r11], %[r10], [%[src] + 32]}\n\t"
+        "mulx{q -32(%[src]), %[r10], %[r11]| %[r11], %[r10], [%[src] - 32]}\n\t"
         "adcx{q 24(%[dst]), %[r8]| %[r8], [%[dst] + 24]}\n\t"
         "adox{q %[r9], %[r10]| %[r10], %[r9]}\n\t"
         "mov{q %[r8], 24(%[dst])| [%[dst] + 24], %[r8]}\n\t"
 
         ".Lb4%=:\n\t"
-        "mulx{q 40(%[src]), %[r8], %[r9]| %[r9], %[r8], [%[src] + 40]}\n\t"
-        "adcx{q 32(%[dst]), %[r10]| %[r10], [%[dst] + 32]}\n\t"
+        "mulx{q -24(%[src]), %[r8], %[r9]| %[r9], %[r8], [%[src] - 24]}\n\t"
         "adox{q %[r11], %[r8]| %[r8], %[r11]}\n\t"
+        "adcx{q 32(%[dst]), %[r10]| %[r10], [%[dst] + 32]}\n\t"
         "mov{q %[r10], 32(%[dst])| [%[dst] + 32], %[r10]}\n\t"
 
         ".Lb3%=:\n\t"
-        "mulx{q 48(%[src]), %[r10], %[r11]| %[r11], %[r10], [%[src] + 48]}\n\t"
+        "mulx{q -16(%[src]), %[r10], %[r11]| %[r11], %[r10], [%[src] - 16]}\n\t"
         "adcx{q 40(%[dst]), %[r8]| %[r8], [%[dst] + 40]}\n\t"
         "adox{q %[r9], %[r10]| %[r10], %[r9]}\n\t"
         "mov{q %[r8], 40(%[dst])| [%[dst] + 40], %[r8]}\n\t"
 
         ".Lb2%=:\n\t"
-        "mulx{q 56(%[src]), %[r8], %[r9]| %[r9], %[r8], [%[src] + 56]}\n\t"
-        "adcx{q 48(%[dst]), %[r10]| %[r10], [%[dst] + 48]}\n\t"
+        "mulx{q -8(%[src]), %[r8], %[r9]| %[r9], %[r8], [%[src] - 8]}\n\t"
         "adox{q %[r11], %[r8]| %[r8], %[r11]}\n\t"
+        "adcx{q 48(%[dst]), %[r10]| %[r10], [%[dst] + 48]}\n\t"
         "mov{q %[r10], 48(%[dst])| [%[dst] + 48], %[r10]}\n\t"
 
-        "lea{q 64(%[src]), %[src]| %[src], [%[src] + 64]}\n\t"
         "lea{q 64(%[dst]), %[dst]| %[dst], [%[dst] + 64]}\n\t"
 
         "jrcxz .Lloop_out%=\n\t"
@@ -382,11 +381,7 @@ inline uint64_t asm_addmul_1(uint64_t *dst, const uint64_t *src, size_t n, uint6
 #endif
 
 #if WJR_HAS_BUILTIN(ASM_MUL_1) && WJR_HAS_BUILTIN(ASM_ADDMUL_1)
-#define WJR_HAS_BUILTIN_ASM_BASECASE_MUL_S WJR_HAS_DEF
-#endif
-
-#if WJR_HAS_BUILTIN(ASM_BASECASE_MUL_S)
-// TODO : low priority
+// #define WJR_HAS_BUILTIN_ASM_BASECASE_MUL_S WJR_HAS_DEF
 #endif
 
 #if WJR_HAS_BUILTIN(ASM_ADDMUL_1)
@@ -433,28 +428,28 @@ inline uint64_t asm_submul_1(uint64_t *dst, const uint64_t *src, size_t n, uint6
 
         ".Ll2%=:\n\t"
         "mulx{q (%[src]), %[r10], %[r11]| %[r11], %[r10], [%[src]]}\n\t"
-        "lea{q -48(%[src]), %[src]| %[src], [%[src] - 48]}\n\t"
+        "lea{q 16(%[src]), %[src]| %[src], [%[src] + 16]}\n\t"
         "not %[r10]\n\t"
         "lea{q -48(%[dst]), %[dst]| %[dst], [%[dst] - 48]}\n\t"
         "jmp .Lb2%=\n\t"
 
         ".Ll3%=:\n\t"
         "mulx{q (%[src]), %[r8], %[r9]| %[r9], %[r8], [%[src]]}\n\t"
-        "lea{q -40(%[src]), %[src]| %[src], [%[src] - 40]}\n\t"
+        "lea{q 24(%[src]), %[src]| %[src], [%[src] + 24]}\n\t"
         "not %[r8]\n\t"
         "lea{q -40(%[dst]), %[dst]| %[dst], [%[dst] - 40]}\n\t"
         "jmp .Lb3%=\n\t"
 
         ".Ll4%=:\n\t"
         "mulx{q (%[src]), %[r10], %[r11]| %[r11], %[r10], [%[src]]}\n\t"
-        "lea{q -32(%[src]), %[src]| %[src], [%[src] - 32]}\n\t"
+        "lea{q 32(%[src]), %[src]| %[src], [%[src] + 32]}\n\t"
         "not %[r10]\n\t"
         "lea{q -32(%[dst]), %[dst]| %[dst], [%[dst] - 32]}\n\t"
         "jmp .Lb4%=\n\t"
 
         ".Ll5%=:\n\t"
         "mulx{q (%[src]), %[r8], %[r9]| %[r9], %[r8], [%[src]]}\n\t"
-        "lea{q -24(%[src]), %[src]| %[src], [%[src] - 24]}\n\t"
+        "lea{q 40(%[src]), %[src]| %[src], [%[src] + 40]}\n\t"
         "not %[r8]\n\t"
         "lea{q -24(%[dst]), %[dst]| %[dst], [%[dst] - 24]}\n\t"
         "jmp .Lb5%=\n\t"
@@ -498,10 +493,10 @@ inline uint64_t asm_submul_1(uint64_t *dst, const uint64_t *src, size_t n, uint6
 
         ".Lb0%=:\n\t"
         "mulx{q 8(%[src]), %[r8], %[r9]| %[r9], %[r8], [%[src] + 8]}\n\t"
-        "adcx{q (%[dst]), %[r10]| %[r10], [%[dst]]}\n\t"
         "adox{q %[r11], %[r8]| %[r8], %[r11]}\n\t"
-        "mov{q %[r10], (%[dst])| [%[dst]], %[r10]}\n\t"
+        "adcx{q (%[dst]), %[r10]| %[r10], [%[dst]]}\n\t"
         "not %[r8]\n\t"
+        "mov{q %[r10], (%[dst])| [%[dst]], %[r10]}\n\t"
 
         "lea{q -1(%[cx]), %[cx]| %[cx], [%[cx] - 1]}\n\t"
 
@@ -514,40 +509,40 @@ inline uint64_t asm_submul_1(uint64_t *dst, const uint64_t *src, size_t n, uint6
 
         ".Lb6%=:\n\t"
         "mulx{q 24(%[src]), %[r8], %[r9]| %[r9], %[r8], [%[src] + 24]}\n\t"
-        "adcx{q 16(%[dst]), %[r10]| %[r10], [%[dst] + 16]}\n\t"
+        "lea{q 64(%[src]), %[src]| %[src], [%[src] + 64]}\n\t"
         "adox{q %[r11], %[r8]| %[r8], %[r11]}\n\t"
-        "mov{q %[r10], 16(%[dst])| [%[dst] + 16], %[r10]}\n\t"
+        "adcx{q 16(%[dst]), %[r10]| %[r10], [%[dst] + 16]}\n\t"
         "not %[r8]\n\t"
+        "mov{q %[r10], 16(%[dst])| [%[dst] + 16], %[r10]}\n\t"
 
         ".Lb5%=:\n\t"
-        "mulx{q 32(%[src]), %[r10], %[r11]| %[r11], %[r10], [%[src] + 32]}\n\t"
+        "mulx{q -32(%[src]), %[r10], %[r11]| %[r11], %[r10], [%[src] - 32]}\n\t"
         "adcx{q 24(%[dst]), %[r8]| %[r8], [%[dst] + 24]}\n\t"
         "adox{q %[r9], %[r10]| %[r10], %[r9]}\n\t"
         "mov{q %[r8], 24(%[dst])| [%[dst] + 24], %[r8]}\n\t"
         "not %[r10]\n\t"
 
         ".Lb4%=:\n\t"
-        "mulx{q 40(%[src]), %[r8], %[r9]| %[r9], %[r8], [%[src] + 40]}\n\t"
-        "adcx{q 32(%[dst]), %[r10]| %[r10], [%[dst] + 32]}\n\t"
+        "mulx{q -24(%[src]), %[r8], %[r9]| %[r9], %[r8], [%[src] - 24]}\n\t"
         "adox{q %[r11], %[r8]| %[r8], %[r11]}\n\t"
-        "mov{q %[r10], 32(%[dst])| [%[dst] + 32], %[r10]}\n\t"
+        "adcx{q 32(%[dst]), %[r10]| %[r10], [%[dst] + 32]}\n\t"
         "not %[r8]\n\t"
+        "mov{q %[r10], 32(%[dst])| [%[dst] + 32], %[r10]}\n\t"
 
         ".Lb3%=:\n\t"
-        "mulx{q 48(%[src]), %[r10], %[r11]| %[r11], %[r10], [%[src] + 48]}\n\t"
+        "mulx{q -16(%[src]), %[r10], %[r11]| %[r11], %[r10], [%[src] - 16]}\n\t"
         "adcx{q 40(%[dst]), %[r8]| %[r8], [%[dst] + 40]}\n\t"
         "adox{q %[r9], %[r10]| %[r10], %[r9]}\n\t"
         "mov{q %[r8], 40(%[dst])| [%[dst] + 40], %[r8]}\n\t"
         "not %[r10]\n\t"
 
         ".Lb2%=:\n\t"
-        "mulx{q 56(%[src]), %[r8], %[r9]| %[r9], %[r8], [%[src] + 56]}\n\t"
-        "adcx{q 48(%[dst]), %[r10]| %[r10], [%[dst] + 48]}\n\t"
+        "mulx{q -8(%[src]), %[r8], %[r9]| %[r9], %[r8], [%[src] - 8]}\n\t"
         "adox{q %[r11], %[r8]| %[r8], %[r11]}\n\t"
-        "mov{q %[r10], 48(%[dst])| [%[dst] + 48], %[r10]}\n\t"
+        "adcx{q 48(%[dst]), %[r10]| %[r10], [%[dst] + 48]}\n\t"
         "not %[r8]\n\t"
+        "mov{q %[r10], 48(%[dst])| [%[dst] + 48], %[r10]}\n\t"
 
-        "lea{q 64(%[src]), %[src]| %[src], [%[src] + 64]}\n\t"
         "lea{q 64(%[dst]), %[dst]| %[dst], [%[dst] + 64]}\n\t"
 
         "jrcxz .Lloop_out%=\n\t"
@@ -555,8 +550,8 @@ inline uint64_t asm_submul_1(uint64_t *dst, const uint64_t *src, size_t n, uint6
         ".Lloop_out%=:\n\t"
 
         "adcx{q -8(%[dst]), %[r8]| %[r8], [%[dst] - 8]}\n\t"
-        "adox{q %[cx], %[r9]| %[r9], %[cx]}\n\t"
         "mov{q %[r8], -8(%[dst])| [%[dst] - 8], %[r8]}\n\t"
+        "adox{q %[cx], %[r9]| %[r9], %[cx]}\n\t"
         "sbb{q $-1, %[r9]| %[r9], -1}\n\t"
 
         ".Ldone%=:"
