df73e0de7af6a07d16c9a784b9b77cb18c68a8aa
wjr <1966336874@qq.com>
2024-08-02 01:08:30 +0800

    moved

--- include/wjr/x86/format/utf-8.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef WJR_X86_FORMAT_UTF_8_HPP__
-#define WJR_X86_FORMAT_UTF_8_HPP__
-
-#include <wjr/x86/simd/simd.hpp>
-
-namespace wjr::utf8 {} // namespace wjr::utf8
-
-#endif                 // WJR_X86_FORMAT_UTF_8_HPP__
---
+++ include/wjr/x86/format/utf8/utf8.hpp
@@ -0,0 +1,8 @@
+#ifndef WJR_X86_FORMAT_UTF8_HPP__
+#define WJR_X86_FORMAT_UTF8_HPP__
+
+#include <wjr/x86/simd/simd.hpp>
+
+namespace wjr::utf8 {} // namespace wjr::utf8
+
+#endif                 // WJR_X86_FORMAT_UTF8_HPP__
