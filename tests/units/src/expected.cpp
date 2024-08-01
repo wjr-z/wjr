7b951e788fe1ed99308719fb79cf5af048439a3a
wjr <1966336874@qq.com>
2024-08-02 01:13:07 +0800

    rename

--- tests/units/src/expected.cpp
+++ tests/units/src/expected.cpp
@@ -16,8 +16,9 @@ TEST(expected, trivial) {
     }
 
     {
-        using type = expected<int, inlined_unexpected<bool, true>>;
+        using type = compressed_expected<int, bool, true>;
 
+        static_assert(sizeof(type) == sizeof(int) * 2, "");
         static_assert(std::is_trivially_destructible_v<type>, "");
         static_assert(std::is_trivially_copy_constructible_v<type>, "");
         static_assert(std::is_trivially_move_constructible_v<type>, "");
