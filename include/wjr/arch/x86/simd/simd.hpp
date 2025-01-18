24d372d4043d7632dfab1b5d5cb9b9bd6da95ac7
wjr <1966336874@qq.com>
2025-01-18 13:12:39 +0800

    Add comment

--- include/wjr/arch/x86/simd/simd.hpp
+++ include/wjr/arch/x86/simd/simd.hpp
@@ -1,3 +1,16 @@
+/**
+ * @file simd.hpp
+ * @author wjr
+ * @brief
+ * @version 0.1
+ * @date 2025-01-18
+ *
+ * @todo
+ *
+ * @copyright Copyright (c) 2025
+ *
+ */
+
 #ifndef WJR_ARCH_X86_SIMD_SIMD_HPP__
 #define WJR_ARCH_X86_SIMD_SIMD_HPP__
 
