a94787907a6775076ed303fb70891dc2e1bccb13
wjr <1966336874@qq.com>
2025-01-18 17:57:54 +0800

    Update CMake.

--- src/wjr/arch/x86/cpuinfo.cpp
+++ src/wjr/arch/x86/cpuinfo.cpp
@@ -1,5 +1,5 @@
-#include <limits>
 #include <cstdint>
+#include <limits>
 
 #include <wjr/arch/x86/cpuinfo.hpp>
 
@@ -12,10 +12,12 @@ cpu_features::X86Info x86_cpu_info = cpu_features::GetX86Info();
 
 inline constexpr size_t __x86_rep_stosb_default_threshold = WJR_X86_REP_STOSB_DEFAULT_THRESHOLD;
 
+namespace {
 template <typename T>
 inline size_t __x86_rep_stosb_threshold_impl =
     x86_cpu_info.features.fs_rep_stosb ? __x86_rep_stosb_default_threshold / sizeof(T)
                                        : std::numeric_limits<size_t>::max();
+}
 
 size_t __x86_rep_stosb_threshold_u16 = __x86_rep_stosb_threshold_impl<uint16_t>;
 size_t __x86_rep_stosb_threshold_u32 = __x86_rep_stosb_threshold_impl<uint32_t>;
