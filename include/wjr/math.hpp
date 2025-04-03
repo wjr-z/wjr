cdcf94b52a8b662f10e753e7b644c75c9f779543
wjr <1966336874@qq.com>
2025-04-03 10:40:13 +0800

    Fix bug

--- include/wjr/math.hpp
+++ include/wjr/math.hpp
@@ -6,9 +6,11 @@
 #include <wjr/math/broadcast.hpp>
 #include <wjr/math/div.hpp>
 #include <wjr/math/div1by1.hpp>
+#include <wjr/math/find.hpp>
 #include <wjr/math/integral_constant.hpp>
 #include <wjr/math/literals.hpp>
 #include <wjr/math/prefix_xor.hpp>
 #include <wjr/math/random.hpp>
+#include <wjr/math/replace.hpp>
 
 #endif // WJR_MATH_HPP__
