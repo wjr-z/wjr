9847cedfd410be5161bfe97c6655a2e7eef7b51c
wjr <1966336874@qq.com>
2025-09-15 22:10:55 +0800

    Remove useless wjr/crtp.hpp, use wjr/crtp/... instead. Move path of variant.hpp.

--- include/wjr/crtp.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef WJR_CRTP_HPP__
-#define WJR_CRTP_HPP__
-
-#include <wjr/crtp/class_base.hpp>
-#include <wjr/crtp/nonsendable.hpp>
-#include <wjr/crtp/trivially_allocator_base.hpp>
-
-#endif // WJR_CRTP_HPP__
