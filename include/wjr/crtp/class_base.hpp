3721a93c1eb44e39f09cc1f2a191af06088a89db
wjr <1966336874@qq.com>
2025-11-15 12:46:44 +0800

    Fix concepts

--- include/wjr/crtp/class_base.hpp
+++ include/wjr/crtp/class_base.hpp
@@ -23,64 +23,175 @@ struct enable_default_constructor_t {
 
 inline constexpr enable_default_constructor_t enable_default_constructor{};
 
-template <bool Default, bool Destructor, bool Copy, bool Move, bool CopyAssign, bool MoveAssign,
-          typename Tag = void>
-struct WJR_EMPTY_BASES enable_special_members_base {
-    enable_special_members_base()
-    requires(Default)
-    = default;
-
-    enable_special_members_base()
-    requires(!Default)
-    = delete;
-
-    enable_special_members_base(const enable_special_members_base &)
-    requires(Copy)
-    = default;
-
-    enable_special_members_base(const enable_special_members_base &)
-    requires(!Copy)
-    = delete;
-
-    enable_special_members_base(enable_special_members_base &&)
-    requires(Move)
-    = default;
-
-    enable_special_members_base(enable_special_members_base &&)
-    requires(!Move)
-    = delete;
-
-    enable_special_members_base &operator=(const enable_special_members_base &)
-    requires(CopyAssign)
-    = default;
-
-    enable_special_members_base &operator=(const enable_special_members_base &)
-    requires(!CopyAssign)
-    = delete;
-
-    enable_special_members_base &operator=(enable_special_members_base &&)
-    requires(MoveAssign)
-    = default;
-
-    enable_special_members_base &operator=(enable_special_members_base &&)
-    requires(!MoveAssign)
-    = delete;
-
-    ~enable_special_members_base()
-    requires(Destructor)
-    = default;
-
-    ~enable_special_members_base()
-    requires(!Destructor)
-    = delete;
+template <bool Enable, typename Tag>
+struct enable_default_constructor_base {
+    enable_default_constructor_base() = default;
+    enable_default_constructor_base(const enable_default_constructor_base &) = default;
+    enable_default_constructor_base(enable_default_constructor_base &&) = default;
+    enable_default_constructor_base &operator=(const enable_default_constructor_base &) = default;
+    enable_default_constructor_base &operator=(enable_default_constructor_base &&) = default;
 
 protected:
-    constexpr explicit enable_special_members_base(enable_default_constructor_t) noexcept {}
+    constexpr explicit enable_default_constructor_base(enable_default_constructor_t) noexcept {}
 };
 
-template <bool Default, typename Tag = void>
-using enable_default_constructor_base =
-    enable_special_members_base<Default, true, true, true, true, true, Tag>;
+template <typename Tag>
+struct enable_default_constructor_base<false, Tag> {
+    enable_default_constructor_base() = delete;
+    enable_default_constructor_base(const enable_default_constructor_base &) = default;
+    enable_default_constructor_base(enable_default_constructor_base &&) = default;
+    enable_default_constructor_base &operator=(const enable_default_constructor_base &) = default;
+    enable_default_constructor_base &operator=(enable_default_constructor_base &&) = default;
+
+protected:
+    constexpr explicit enable_default_constructor_base(enable_default_constructor_t) noexcept {}
+};
+
+template <bool Enable, typename Tag>
+struct enable_copy_constructor_base {
+    enable_copy_constructor_base() = default;
+    enable_copy_constructor_base(const enable_copy_constructor_base &) = default;
+    enable_copy_constructor_base(enable_copy_constructor_base &&) = default;
+    enable_copy_constructor_base &operator=(const enable_copy_constructor_base &) = default;
+    enable_copy_constructor_base &operator=(enable_copy_constructor_base &&) = default;
+
+protected:
+    constexpr explicit enable_copy_constructor_base(enable_default_constructor_t) noexcept {}
+};
+
+template <typename Tag>
+struct enable_copy_constructor_base<false, Tag> {
+    enable_copy_constructor_base() = default;
+    enable_copy_constructor_base(const enable_copy_constructor_base &) = delete;
+    enable_copy_constructor_base(enable_copy_constructor_base &&) = default;
+    enable_copy_constructor_base &operator=(const enable_copy_constructor_base &) = default;
+    enable_copy_constructor_base &operator=(enable_copy_constructor_base &&) = default;
+
+protected:
+    constexpr explicit enable_copy_constructor_base(enable_default_constructor_t) noexcept {}
+};
+
+template <bool Enable, typename Tag>
+struct enable_move_constructor_base {
+    enable_move_constructor_base() = default;
+    enable_move_constructor_base(const enable_move_constructor_base &) = default;
+    enable_move_constructor_base(enable_move_constructor_base &&) = default;
+    enable_move_constructor_base &operator=(const enable_move_constructor_base &) = default;
+    enable_move_constructor_base &operator=(enable_move_constructor_base &&) = default;
+
+protected:
+    constexpr explicit enable_move_constructor_base(enable_default_constructor_t) noexcept {}
+};
+
+template <typename Tag>
+struct enable_move_constructor_base<false, Tag> {
+    enable_move_constructor_base() = default;
+    enable_move_constructor_base(const enable_move_constructor_base &) = default;
+    enable_move_constructor_base(enable_move_constructor_base &&) = delete;
+    enable_move_constructor_base &operator=(const enable_move_constructor_base &) = default;
+    enable_move_constructor_base &operator=(enable_move_constructor_base &&) = default;
+
+protected:
+    constexpr explicit enable_move_constructor_base(enable_default_constructor_t) noexcept {}
+};
+
+template <bool Enable, typename Tag>
+struct enable_copy_assignment_base {
+    enable_copy_assignment_base() = default;
+    enable_copy_assignment_base(const enable_copy_assignment_base &) = default;
+    enable_copy_assignment_base(enable_copy_assignment_base &&) = default;
+    enable_copy_assignment_base &operator=(const enable_copy_assignment_base &) = default;
+    enable_copy_assignment_base &operator=(enable_copy_assignment_base &&) = default;
+
+protected:
+    constexpr explicit enable_copy_assignment_base(enable_default_constructor_t) noexcept {}
+};
+
+template <typename Tag>
+struct enable_copy_assignment_base<false, Tag> {
+    enable_copy_assignment_base() = default;
+    enable_copy_assignment_base(const enable_copy_assignment_base &) = default;
+    enable_copy_assignment_base(enable_copy_assignment_base &&) = default;
+    enable_copy_assignment_base &operator=(const enable_copy_assignment_base &) = delete;
+    enable_copy_assignment_base &operator=(enable_copy_assignment_base &&) = default;
+
+protected:
+    constexpr explicit enable_copy_assignment_base(enable_default_constructor_t) noexcept {}
+};
+
+template <bool Enable, typename Tag>
+struct enable_move_assignment_base {
+    enable_move_assignment_base() = default;
+    enable_move_assignment_base(const enable_move_assignment_base &) = default;
+    enable_move_assignment_base(enable_move_assignment_base &&) = default;
+    enable_move_assignment_base &operator=(const enable_move_assignment_base &) = default;
+    enable_move_assignment_base &operator=(enable_move_assignment_base &&) = default;
+
+protected:
+    constexpr explicit enable_move_assignment_base(enable_default_constructor_t) noexcept {}
+};
+
+template <typename Tag>
+struct enable_move_assignment_base<false, Tag> {
+    enable_move_assignment_base() = default;
+    enable_move_assignment_base(const enable_move_assignment_base &) = default;
+    enable_move_assignment_base(enable_move_assignment_base &&) = default;
+    enable_move_assignment_base &operator=(const enable_move_assignment_base &) = default;
+    enable_move_assignment_base &operator=(enable_move_assignment_base &&) = delete;
+
+protected:
+    constexpr explicit enable_move_assignment_base(enable_default_constructor_t) noexcept {}
+};
+
+template <bool Enable, typename Tag>
+struct enable_destructor_base {
+    enable_destructor_base() = default;
+    enable_destructor_base(const enable_destructor_base &) = default;
+    enable_destructor_base(enable_destructor_base &&) = default;
+    enable_destructor_base &operator=(const enable_destructor_base &) = default;
+    enable_destructor_base &operator=(enable_destructor_base &&) = default;
+    ~enable_destructor_base() = default;
+
+protected:
+    constexpr explicit enable_destructor_base(enable_default_constructor_t) noexcept {}
+};
+
+template <typename Tag>
+struct enable_destructor_base<false, Tag> {
+    enable_destructor_base() = default;
+    enable_destructor_base(const enable_destructor_base &) = default;
+    enable_destructor_base(enable_destructor_base &&) = default;
+    enable_destructor_base &operator=(const enable_destructor_base &) = default;
+    enable_destructor_base &operator=(enable_destructor_base &&) = default;
+    ~enable_destructor_base() = delete;
+
+protected:
+    constexpr explicit enable_destructor_base(enable_default_constructor_t) noexcept {}
+};
+
+template <bool Default, bool Destructor, bool Copy, bool Move, bool CopyAssign, bool MoveAssign,
+          typename Tag = void>
+struct WJR_EMPTY_BASES enable_special_members_base : enable_default_constructor_base<Default, Tag>,
+                                                     enable_destructor_base<Destructor, Tag>,
+                                                     enable_copy_constructor_base<Copy, Tag>,
+                                                     enable_move_constructor_base<Move, Tag>,
+                                                     enable_copy_assignment_base<CopyAssign, Tag>,
+                                                     enable_move_assignment_base<MoveAssign, Tag> {
+
+private:
+    using Mybase = enable_default_constructor_base<Default, Tag>;
+
+public:
+    enable_special_members_base() = default;
+    enable_special_members_base(const enable_special_members_base &) = default;
+    enable_special_members_base(enable_special_members_base &&) = default;
+    enable_special_members_base &operator=(const enable_special_members_base &) = default;
+    enable_special_members_base &operator=(enable_special_members_base &&) = default;
+
+protected:
+    constexpr explicit enable_special_members_base(enable_default_constructor_t) noexcept
+        : Mybase(enable_default_constructor) {}
+};
 
 template <bool Copy, bool Move, bool CopyAssign, bool MoveAssign, typename Tag = void>
 using enable_copy_move_base =
