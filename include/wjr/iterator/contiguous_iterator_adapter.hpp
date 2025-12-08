6653b0cb7b786f03c55f57226102ffdce01e3f2c
wjr <1966336874@qq.com>
2025-12-08 21:50:55 +0800

    print end

--- include/wjr/iterator/contiguous_iterator_adapter.hpp
+++ include/wjr/iterator/contiguous_iterator_adapter.hpp
@@ -48,7 +48,9 @@ public:
         //           "Can't dereference an out-of-range iterator.");
         if (m_container != nullptr && m_ptr != nullptr) {
             auto s = _begin();
+            auto e = _end();
             std::cout << "begin: " << static_cast<const void *>(s) << std::endl;
+            std::cout << "end: " << static_cast<const void *>(e) << std::endl;
         }
 #endif
         return const_cast<pointer>(m_ptr);
